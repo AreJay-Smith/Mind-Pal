c3c602c5ceb95b8abf10d52c8c821a5b5396f143
RJ <randalljo@gmail.com>
2019-07-11 20:31:44 -0600

    Timer and Progressbar still not working

--- MindPal/Practice/GameSession.swift
+++ MindPal/Practice/GameSession.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+class GameSession {
+    
+    var allCards = ["2C","3C","4C","5C","6C","7C","8C","9C","10C","JC","QC","KC","AC","2S","3S","4S","5S","6S","7S","8S","9S","10S","JS","QS","KS","AS","2H","3H","4H","5H","6H","7H","8H","9H","10H","JH","QH","KH","AH","2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD","AD"]
+    var selectedCards: Array<String> = []
+    var currentCardIndex = 0
+    
+    init(_ numCards: Int) {
+        generateSelectedList(with: numCards)
+    }
+    
+    func generateSelectedList(with numCards: Int) {
+        if (numCards != 0) {
+        for _ in 1...numCards {
+            let randomCardNumber = Int.random(in: 0...allCards.count)
+            selectedCards.append(allCards[randomCardNumber])
+            allCards.remove(at: randomCardNumber)
+        }
+        }
+    }
+    
+    func nextCardInDeck() {
+        if currentCardIndex < selectedCards.count-1 {
+            currentCardIndex += 1
+        }
+    }
+    
+    func backCardInDeck() {
+        if currentCardIndex > 0 {
+            currentCardIndex -= 1
+        }
+    }
+    
+    func getCurrentCardName() -> String {
+        return selectedCards[currentCardIndex]
+    }
+}
--- MindPal/Practice/PracticeViewController.swift
+++ MindPal/Practice/PracticeViewController.swift
@@ -12,13 +12,58 @@ class PracticeViewController: UIViewController {
     
     var numCards = 0
     var numMinutes = 0
+    var gameSession: GameSession?
+    
+    var seconds = 60
+    var timer = Timer()
+    var isTimerRunning = false
 
+    @IBOutlet weak var leftArrow: UIImageView!
+    @IBOutlet weak var rightArrow: UIImageView!
+    @IBOutlet weak var currentRenderedCard: UIImageView!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var cardCountLabel: UILabel!
+    @IBOutlet weak var progressBar: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print(numCards)
-        print(numMinutes)
         // Do any additional setup after loading the view.
         self.navigationController?.viewControllers.remove(at: 1)
+        
+        gameSession = GameSession(numCards)
+        updateCurrentCard()
+        
+        leftArrow.isUserInteractionEnabled = true
+        leftArrow.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.leftTap)))
+        rightArrow.isUserInteractionEnabled = true
+        rightArrow.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.rightTap)))
+        
+//        runTimer()
+    }
+    
+    // Func in your UIViewController
+    @objc func leftTap() {
+        gameSession!.backCardInDeck()
+        updateCurrentCard()
+    }
+    @objc func rightTap() {
+        gameSession!.nextCardInDeck()
+        updateCurrentCard()
+    }
+    
+    private func updateCurrentCard() {
+        currentRenderedCard.image = UIImage(named: gameSession!.getCurrentCardName())
+        cardCountLabel.text = "\(gameSession!.currentCardIndex + 1) / \(gameSession!.selectedCards.count)"
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(gameSession!.currentCardIndex+1)
+    }
+    
+//    private func runTimer() {
+//        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+//    }
+    
+    func updateTimer() {
+        seconds -= 1     //This will decrement(count down)the seconds.
+        timerLabel.text = "\(seconds)" //This will update the label.
     }
 }
